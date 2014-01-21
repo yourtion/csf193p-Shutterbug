2c7f6438dcdc62729c43ce508fb18f01e1cd75ec
Yourtion <yourtion@gmail.com>
2014-01-21 20:58:26 +0800

    Add flickphotoTVC

---
+++ Shutterbug/FlickPhotoTVC.h
@@ -0,0 +1,13 @@
+//
+//  FlickPhotoTVC.h
+//  Shutterbug
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickPhotoTVC : UITableViewController
+@property (nonatomic, strong) NSArray *photos; // of Flick photos NSDictionary
+@end
---
+++ Shutterbug/FlickPhotoTVC.m
@@ -0,0 +1,61 @@
+//
+//  FlickPhotoTVC.m
+//  Shutterbug
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import "FlickPhotoTVC.h"
+#import "FlickrFetcher.h"
+
+@interface FlickPhotoTVC ()
+
+@end
+
+@implementation FlickPhotoTVC
+
+-(void)setPhotos:(NSArray *)photos
+{
+    _photos = photos;
+    [self.tableView reloadData];
+}
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.photos count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"FlickrPhotoCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    NSDictionary *photo = self.photos[indexPath.row];
+    cell.textLabel.text = [photo valueForKeyPath:FLICKR_PHOTO_TITLE];
+    cell.detailTextLabel.text = [photo valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
+    
+    return cell;
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a story board-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ 
+ */
+@end
