daf87f3868641c00e2d0b6e3b7a7d0c9825abbb6
Yourtion <yourtion@gmail.com>
2014-01-21 21:22:00 +0800

    Add imageViewController

---
+++ Shutterbug/imageViewController.h
@@ -0,0 +1,13 @@
+//
+//  imageViewController.h
+//  Imaginarium
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface imageViewController : UIViewController
+@property (strong ,nonatomic)NSURL *imageURL;
+@end
