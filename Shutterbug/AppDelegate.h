ddaa506c870dd3c80d4c257e0fb8977161c5c6c7
Yourtion <yourtion@gmail.com>
2014-01-21 19:13:48 +0800

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
